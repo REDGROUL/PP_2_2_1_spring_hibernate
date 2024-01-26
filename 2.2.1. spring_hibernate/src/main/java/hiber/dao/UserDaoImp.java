c70328ecce8170c1564ed24ef962a60a0ad84691
RED <redgroulman@gmail.com>
2024-01-26 09:22:36 +0300

    Исправленил в UserDaoImpl @Autowired в двух местах

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -12,33 +12,32 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
 
+    private SessionFactory sessionFactory;
 
     @Autowired
     public UserDaoImp(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-    public List<User> findByModelAndSeries(String model, int series){
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    public List<User> findByModelAndSeries(String model, int series) {
         Query query = sessionFactory.getCurrentSession().createQuery("from User where car.model = (:model) and car.series = (:series)");
         query.setParameter("model", model);
         query.setParameter("series", series);
         return query.getResultList();
     }
-
 
 
 }
